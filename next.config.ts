698e9315429be45d75d8d529eb7f300f1c1d0e39
YuriLRodrigues <rodriguesyuri769@gmail.com>
2025-03-30 18:50:25 -0300

    fix: :bug: env enviroment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+  },
+}
 
-export default nextConfig;
+export default nextConfig
